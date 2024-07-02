bb1cf3d3719ea6e86b751e0e67e2d9a0ab1d853e
IAmSpade <34572462+RelativeSpade@users.noreply.github.com>
2024-07-02 06:38:52 -0400

    basic saving + checking environment

--- Script/Main.py
+++ Script/Main.py
@@ -8,6 +8,16 @@ import cv2
 import os
 from stable_baselines3.common.callbacks import BaseCallback
 
+from Script.TrainAndLoggingCallback import TrainAndLoggingCallback
+from stable_baselines3.common import env_checker
+
+from Script.VizDoomGym import VizDoomGym
+
 CHECKPOINT_DIR = '../train/'
 LOG_DIR = '../logs/'
 
+callback = TrainAndLoggingCallback(check_freq=10000, save_path=CHECKPOINT_DIR)
+
+env = VizDoomGym(render=False)
+
+env_checker.check_env(env)
