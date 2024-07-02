465970afeb28b95c6afc0f43aafe122d5a242cbe
IAmSpade <34572462+RelativeSpade@users.noreply.github.com>
2024-07-02 06:49:43 -0400

    untested basic model finished

--- Script/Main.py
+++ Script/Main.py
@@ -1,16 +1,6 @@
-from vizdoom import *
-import random
-import time
-import numpy as np
-from gym import Env
-from gym.spaces import Discrete, Box
-import cv2
-import os
-from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3 import PPO
 
 from Script.TrainAndLoggingCallback import TrainAndLoggingCallback
-from stable_baselines3.common import env_checker
-
 from Script.VizDoomGym import VizDoomGym
 
 CHECKPOINT_DIR = '../train/'
@@ -18,6 +8,8 @@ LOG_DIR = '../logs/'
 
 callback = TrainAndLoggingCallback(check_freq=10000, save_path=CHECKPOINT_DIR)
 
-env = VizDoomGym(render=False)
+env = VizDoomGym()
 
-env_checker.check_env(env)
+model = PPO('CnnPolicy', env, tensorboard_log=LOG_DIR, verbose=1, learning_rate=0.0001, n_steps=256)
+
+model.learn(total_timesteps=100000, callback=callback)
