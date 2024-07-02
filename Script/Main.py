7909089d995367dae2a2b68ec996273f76537fd9
IAmSpade <34572462+RelativeSpade@users.noreply.github.com>
2024-07-02 10:13:30 -0400

    Fixed model resuming

--- Script/Main.py
+++ Script/Main.py
@@ -13,7 +13,6 @@ env = VizDoomGym(False, '../Vizdoom/scenarios/defend_the_center.cfg')
 
 
 def get_latest_model(checkpoint_dir):
-    # Regex to match the model filenames and extract the iteration number
     pattern = re.compile(r'Doom(\d+)')
     checkpoint_files = [
         (file, int(pattern.search(file).group(1)))
@@ -21,17 +20,18 @@ def get_latest_model(checkpoint_dir):
         if pattern.search(file)
     ]
     if not checkpoint_files:
-        return None
-    # Get the filename with the highest iteration number
-    latest_checkpoint = max(checkpoint_files, key=lambda x: x[1])[0]
-    print('Latest checkpoint:', latest_checkpoint)
-    return os.path.join(checkpoint_dir, latest_checkpoint)
+        return None, 0
+
+    latest_checkpoint = max(checkpoint_files, key=lambda x: x[1])
+    print('Latest checkpoint:', latest_checkpoint[0])
+    return os.path.join(checkpoint_dir, latest_checkpoint[0]), latest_checkpoint[1]
 
 
-latest_model_path = get_latest_model(CHECKPOINT_DIR)
+latest_model_path, latest_n_calls = get_latest_model(CHECKPOINT_DIR)
 if latest_model_path is None:
     model = PPO('CnnPolicy', env, tensorboard_log=LOG_DIR, verbose=1, learning_rate=0.0001, n_steps=4096)
 else:
     model = PPO.load(latest_model_path, env, tensorboard_log=LOG_DIR)
+    callback.n_calls = latest_n_calls
 
-model.learn(total_timesteps=100000, callback=callback)
+model.learn(total_timesteps=latest_n_calls + 100000, callback=callback)
